ae12f4a831ce063dc353773c5b78e57909ce94d3
marcelschurer <marcel.schurer@outlook.de>
2024-11-21 12:35:36 +0100

    update

--- examples/own_simulations/intake_manifold_3d.jl
+++ examples/own_simulations/intake_manifold_3d.jl
@@ -4,8 +4,8 @@ using OrdinaryDiffEq
 # ==========================================================================================
 # ==== Resolution
 
-tspan = (0.0, 0.5)
-particle_spacing = 1.0 * 10^-3 # 1 mm
+tspan = (0.0, 0.1)
+particle_spacing = 3.0 * 10^-3 # 1 mm
 smoothing_length = 3.0 * particle_spacing
 
 # ==========================================================================================
@@ -32,10 +32,13 @@ point_in_geometry_algorithm = WindingNumberJacobson(; geometry,
                                                     hierarchical_winding=true)
 
 # Returns `InitialCondition`
-inlet_sampled = ComplexShape(geometry; particle_spacing, density=1.0,
+inlet_sampled = ComplexShape(geometry; particle_spacing, density=density,
+                             boundary_thickness=5 * particle_spacing,
+                             create_signed_distance_field=true,
+                             sample_boundary=false,
                              point_in_geometry_algorithm)
 
-trixi2vtk(inlet_sampled, filename="inlet")
+#trixi2vtk(inlet_sampled, filename="inlet")
 
 # ==========================================================================================
 # ==== Intake Manifold
@@ -46,14 +49,14 @@ file = joinpath("examples", "preprocessing", "data", "intake_manifold", filename
 geometry = load_geometry(file)
 
 point_in_geometry_algorithm = WindingNumberJacobson(; geometry,
-                                                    # winding_number_factor=0.4,
+                                                    #winding_number_factor=0.4,
                                                     hierarchical_winding=true)
 
 # Returns `InitialCondition`
-intake_manifold_sampled = ComplexShape(geometry; particle_spacing, density=1.0,
+intake_manifold_sampled = ComplexShape(geometry; particle_spacing, density=density,
                                        point_in_geometry_algorithm)
 
-trixi2vtk(intake_manifold_sampled, filename="intake_manifold")
+#trixi2vtk(intake_manifold_sampled, filename="intake_manifold")
 
 # ==========================================================================================
 # ==== Outlet Left
@@ -68,10 +71,10 @@ point_in_geometry_algorithm = WindingNumberJacobson(; geometry,
                                                     hierarchical_winding=true)
 
 # Returns `InitialCondition`
-outlet_left_sampled = ComplexShape(geometry; particle_spacing, density=1.0,
+outlet_left_sampled = ComplexShape(geometry; particle_spacing, density=density,
                                    point_in_geometry_algorithm)
 
-trixi2vtk(outlet_left_sampled, filename="outlet_left")
+#trixi2vtk(outlet_left_sampled, filename="outlet_left")
 
 # ==========================================================================================
 # ==== Outlet Right
@@ -86,10 +89,10 @@ point_in_geometry_algorithm = WindingNumberJacobson(; geometry,
                                                     hierarchical_winding=true)
 
 # Returns `InitialCondition`
-outlet_right_sampled = ComplexShape(geometry; particle_spacing, density=1.0,
+outlet_right_sampled = ComplexShape(geometry; particle_spacing, density=density,
                                     point_in_geometry_algorithm)
 
-trixi2vtk(outlet_right_sampled, filename="outlet_right")
+#trixi2vtk(outlet_right_sampled, filename="outlet_right")
 
 # ==========================================================================================
 # ==== Fluid
@@ -99,7 +102,7 @@ fluid_density_calculator = ContinuityDensity()
 
 viscosity = ViscosityAdami(nu=kinematic_viscosity)
 
-n_buffer_particles = 4 * size(intake_manifold_sampled.coordinates, 2)
+n_buffer_particles = 1 * size(intake_manifold_sampled.coordinates, 2)
 
 fluid_system = EntropicallyDampedSPHSystem(intake_manifold_sampled, smoothing_kernel,
                                            smoothing_length,
@@ -108,8 +111,16 @@ fluid_system = EntropicallyDampedSPHSystem(intake_manifold_sampled, smoothing_ke
                                            buffer_size=n_buffer_particles)
 
 # ==========================================================================================
+# ==== Boundary
+boundary_system = ParticlePackingSystem(intake_manifold_sampled; tlsph=tlsph,
+                                        is_boundary=true, background_pressure=pressure)
+
+packing_system = ParticlePackingSystem(intake_manifold_sampled; tlsph=tlsph,
+                                       background_pressure=pressure)
+
+# ==========================================================================================
 # ==== Simulation
-semi = Semidiscretization(fluid_system)
+semi = Semidiscretization(packing_system, boundary_system)
 
 ode = semidiscretize(semi, tspan)
 
